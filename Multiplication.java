92e7fa2c4437fd9204c51f73f82959658df5a4ba
AdedejiArt <71102564+AdedejiArt@users.noreply.github.com>
2021-01-15 00:47:48 -0800

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author Roheem
+ *
+ */
+class multiply 
+{
+public static void main(sring[] args) {
+int a=10,b=5,c;
+c=a*b;
+System.out.printIn("Product of two numbers in javs Is ----->>");
+System.out.print(c);
+}
+}
